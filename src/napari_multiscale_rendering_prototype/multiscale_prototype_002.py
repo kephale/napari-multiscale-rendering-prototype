f29419c97f7561219d057fc42ff2a27c628d6875
Kyle Harrington <czi@kyleharrington.com>
2023-03-23 15:57:18 -0400

    Fetches at multiple z-levels, but rendering is messed up

--- src/napari_multiscale_rendering_prototype/multiscale_prototype_002.py
+++ src/napari_multiscale_rendering_prototype/multiscale_prototype_002.py
@@ -6,15 +6,29 @@ from fibsem_tools import read_xarray
 from ome_zarr.io import parse_url
 from ome_zarr.reader import Reader
 from napari.qt.threading import thread_worker
-from skimage.transform import resize
+from skimage.transform import resize, rescale
+from skimage.util import img_as_uint
 from napari.utils.events import Event
 import dask.array as da
 import sys
 import zarr
 
+import itertools
+
 from napari.utils import config
 
 from napari_multiscale_rendering_prototype.utils import ChunkCacheManager
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Protocol,
+    Tuple,
+    Union,
+    runtime_checkable,
+)
+
+from napari.layers._data_protocols import LayerDataProtocol, Index
 
 global viewer
 viewer = napari.Viewer()
@@ -109,8 +123,8 @@ def get_chunk(
         real_array = np.asarray(
             array[
                 z,
-                y : (y + large_image["chunk_size"][-2]),
-                x : (x + large_image["chunk_size"][-1]),                
+                y : (y + array.data.chunksize[-2]),
+                x : (x + array.data.chunksize[-1]),
             ].compute()
         )
         cache_manager.put(container, dataset, coord, real_array)
@@ -119,63 +133,89 @@ def get_chunk(
 
 
 def chunks_for_scale(corner_pixels, array, scale):
-    # Scale corner pixels to current scale level
-    y1, x1 = corner_pixels[0, :] / (2**scale)
-    y2, x2 = corner_pixels[1, :] / (2**scale)
+    """ Scale corner pixels to current scale level
 
-    z1 = z2 = 150 / (2**scale)
+    This function takes corner pixels, and uses metadata from array/scale
+    to compute slices to display.
+    """
+
+    # TODO all of this needs to be generalized to ND or replaced/merged with volume rendering code
+    
+    mins = corner_pixels[0, :] / (2**scale)
+    maxs = corner_pixels[1, :] / (2**scale)
+
+    chunk_size = array.data.chunksize
+    
+    # z1 = z2 = 150 / (2**scale)
 
     # Find the extent from the current corner pixels, limit by data shape
-    y1 = int(
-        np.floor(y1 / large_image["chunk_size"][0])
-        * large_image["chunk_size"][0]
-    )
-    x1 = int(
-        np.floor(x1 / large_image["chunk_size"][1])
-        * large_image["chunk_size"][1]
-    )
-    z1 = int(
-        np.floor(z1 / large_image["chunk_size"][2])
-        * large_image["chunk_size"][2]
-    )
-    y2 = min(
-        int(
-            np.ceil(y2 / large_image["chunk_size"][0])
-            * large_image["chunk_size"][0]
-        ),
-        array.shape[0],
-    )
-    x2 = min(
-        int(
-            np.ceil(x2 / large_image["chunk_size"][1])
-            * large_image["chunk_size"][1]
-        ),
-        array.shape[1],
-    )
-    z2 = min(
-        int(
-            np.ceil(z2 / large_image["chunk_size"][2])
-            * large_image["chunk_size"][2]
-        ),
-        array.shape[2],
-    )
+    mins = (np.floor(mins / chunk_size) * chunk_size).astype(np.long)
+    maxs = np.min((np.ceil(maxs / chunk_size) * chunk_size, np.array(array.shape)), axis=0).astype(np.long)
 
-    xs = range(x1, x2, large_image["chunk_size"][1])
-    ys = range(y1, y2, large_image["chunk_size"][0])
-    zs = range(z1, z2, large_image["chunk_size"][2])
-
+    xs = range(mins[-1], maxs[-1], chunk_size[-1])
+    ys = range(mins[-2], maxs[-2], chunk_size[-2])
+    zs = range(mins[-3], maxs[-3], chunk_size[-3])
+    
+    # zs = [z1]
+    # TODO kludge
     
     for x in xs:
         for y in ys:
             for z in zs:
-                yield (x, y, z)
+                yield (z, y, x)
 
 
+class VirtualData:
+    def __init__(self, dtype, shape):
+        self.dtype = dtype
+        self.shape = shape
+        self.ndim = len(shape)
+
+        """
+We could call these MarginArrays because they operate on Marginals of dimensions
+However, they are sided.
+SideMargin
+
+We want to be able to reorient our internal data plane.
+        no we should be immutable
+
+        
+        """
+        self.d = 2
+        
+        # TODO: I don't like that this is making a choice of slicing axis
+        self.data_plane = np.zeros(self.shape[-1 * self.d:], dtype=self.dtype)
+
+    def __getitem__(
+        self, key: Union[Index, Tuple[Index, ...], LayerDataProtocol]
+    ) -> LayerDataProtocol:
+        """Returns self[key]."""
+        if type(key) is tuple:
+            return self.data_plane.__getitem__(tuple(key[-1 * self.d:]))
+        else:
+            return self.data_plane.__getitem__(key)
+
+    def __setitem__(
+            self, key: Union[Index, Tuple[Index, ...], LayerDataProtocol],
+            value
+    ) -> LayerDataProtocol:
+        """Returns self[key]."""
+        if type(key) is tuple:
+            return self.data_plane.__setitem__(tuple(key[-1 * self.d:]), value)
+        else:
+            return self.data_plane.__setitem__(key, value)
+        
+
+
+                
 # We ware going to use this np array as our "canvas"
 # TODO at least get this size from the image
 #empty = da.zeros(large_image["arrays"][0].shape[:-1])
 # empty = da.zeros(large_image["arrays"][0].shape, chunks=(1, 512, 512))
-empty = zarr.zeros(large_image["arrays"][0].shape, chunks=(1, 512, 512), dtype=np.uint16)
+# empty = zarr.zeros(large_image["arrays"][0].shape, chunks=(1, 512, 512), dtype=np.uint16)
+
+# TODO make a LayerDataProtocol that fakes the size but only uses a 2D space
+empty = VirtualData(np.uint16, large_image["arrays"][0].shape)
 
 # TODO let's choose a chunk size that matches the axis we'll be looking at
 
@@ -204,92 +244,119 @@ def render_sequence(corner_pixels):
     # NOTE this corner_pixels means something else and should be renamed
     # it is further limited to the visible data on the vispy canvas
 
-    LOGGER.info("render_sequence: inside")
-    
+    LOGGER.info(f"render_sequence: inside with corner pixels {corner_pixels}")
+
+    # TODO scale is hardcoded here
     for scale in reversed(range(4)):
         array = large_image["arrays"][scale]        
         
         chunks_to_fetch = list(chunks_for_scale(corner_pixels, array, scale))
 
-        LOGGER.info(f"render_sequence: {scale}, {array.shape}")
+        LOGGER.info(f"render_sequence: {scale}, {array.shape} fetching {len(chunks_to_fetch)} chunks")        
 
         if num_threads == 1:
             # Single threaded:
-            for (x, y, z) in chunks_to_fetch:
+            for (z, y, x) in chunks_to_fetch:
                 # Trigger a fetch of the data
                 dataset = f"{large_image['dataset']}/s{scale}"
                 LOGGER.info("render_sequence: get_chunk")
                 real_array = get_chunk(
-                    (x, y, z),
+                    (z, y, x),
                     array=array,
                     container=large_image["container"],
                     dataset=dataset,
                     chunk_size=(1, 1, 1),
                 )
                 # Upscale the data to highest resolution
-                upscaled = resize(
-                    real_array, [el * 2**scale for el in real_array.shape]
-                )
-                LOGGER.info(f"yielding: {(x * 2**scale, y * 2**scale, z, scale, upscaled, upscaled.shape)}")
+                # upscaled = resize(
+                #     real_array, [el * 2**scale for el in real_array.shape]
+                # )
+                upscaled = img_as_uint(rescale(real_array, 2**scale))
+                
+                LOGGER.info(f"yielding: {(z * 2**scale, y * 2**scale, x * 2**scale, scale, upscaled.shape)} sample {upscaled[10:20,10]} with sum {upscaled.sum()}")
                 # Return upscaled coordinates, the scale, and chunk
-                yield (x * 2**scale, y * 2**scale, z, scale, upscaled)
-        else:
-            # Make a list of num_threads length sublists
-            job_sets = [
-                chunks_to_fetch[i : i + num_threads]
-                for i in range(0, len(chunks_to_fetch), num_threads)
-            ]
+                chunk_size = upscaled.shape
 
-            dataset = f"{large_image['dataset']}/s{scale}"
+                LOGGER.info(f"yield will be placed at: {(z * 2**scale, y * 2**scale, x * 2**scale, scale, upscaled.shape)}")
+                
+                upscaled_chunk_size = [0, 0]
+                upscaled_chunk_size[0] = min(large_image["arrays"][0].shape[-2] - y * 2**scale, chunk_size[-2])
+                upscaled_chunk_size[1] = min(large_image["arrays"][0].shape[-1] - x * 2**scale, chunk_size[-1])
+                
+                # overflow = np.array(large_image["arrays"][0].shape[-2:]) - (np.array((y, x)) + np.array(chunk_size))
+                
+                # layer.data[z, y : (y + chunk_size[-2]), x : (x + chunk_size[-1])]
+                
+                upscaled = upscaled[:upscaled_chunk_size[-2], :upscaled_chunk_size[-1]]
 
-            def mutable_get_chunk(results, idx, x, y, z):
-                results[idx] = get_chunk(
-                    (x, y, z),
-                    array=array,
-                    container=large_image["container"],
-                    dataset=dataset,
-                )
+                # if sum(upscaled[10:20,10]) > 0:
+                #        import pdb; pdb.set_trace()
+                
+                # if y == 0 and (x * 2**scale) == 7680:
+                #     import pdb; pdb.set_trace()
+                
+                # TODO pickup here, figure out why this is being placed out of bounds and crop it correctly
+                # [z, y : (y + chunk_size[-2]), x : (x + chunk_size[-1])]
 
-            for job_set in job_sets:
-                # Evaluate the jobs in parallel
-                # chunks = p.map(get_chunk, job_set)
-                # We need a mutable result
-                chunks = [None] * len(job_set)
-                # Make the threads
-                threads = [
-                    Thread(
-                        target=mutable_get_chunk,
-                        args=[chunks, idx] + list(args),
-                    )
-                    for idx, args in enumerate(job_set)
-                ]
-                # Start threads
-                for thread in threads:
-                    thread.start()
+                yield (z * 2**scale, y * 2**scale, x * 2**scale, scale, upscaled, upscaled_chunk_size)
+        # TODO all this needs to be updated for (xyz) -> (zyx) change
+        # else:
+        #     # Make a list of num_threads length sublists
+        #     job_sets = [
+        #         chunks_to_fetch[i : i + num_threads]
+        #         for i in range(0, len(chunks_to_fetch), num_threads)
+        #     ]
 
-                # Collect
-                for thread in threads:
-                    thread.join()
+        #     dataset = f"{large_image['dataset']}/s{scale}"
 
-                # Yield the chunks that are done
-                for idx in range(len(chunks)):
-                    # Get job parameters
-                    x, y, z = job_set[idx]
-                    # This contains the real data
-                    real_array = chunks[idx]
-                    # Upscale the data to highest resolution
-                    upscaled = resize(
-                        real_array,
-                        [el * 2**scale for el in real_array.shape],
-                    )
-                    # Return upscaled coordinates, the scale, and chunk
-                    yield (
-                        x * 2**scale,
-                        y * 2**scale,
-                        z,
-                        scale,
-                        upscaled,
-                    )
+        #     def mutable_get_chunk(results, idx, x, y, z):
+        #         results[idx] = get_chunk(
+        #             (x, y, z),
+        #             array=array,
+        #             container=large_image["container"],
+        #             dataset=dataset,
+        #         )
+
+        #     for job_set in job_sets:
+        #         # Evaluate the jobs in parallel
+        #         # chunks = p.map(get_chunk, job_set)
+        #         # We need a mutable result
+        #         chunks = [None] * len(job_set)
+        #         # Make the threads
+        #         threads = [
+        #             Thread(
+        #                 target=mutable_get_chunk,
+        #                 args=[chunks, idx] + list(args),
+        #             )
+        #             for idx, args in enumerate(job_set)
+        #         ]
+        #         # Start threads
+        #         for thread in threads:
+        #             thread.start()
+
+        #         # Collect
+        #         for thread in threads:
+        #             thread.join()
+
+        #         # Yield the chunks that are done
+        #         for idx in range(len(chunks)):
+        #             # Get job parameters
+        #             x, y, z = job_set[idx]
+        #             # This contains the real data
+        #             real_array = chunks[idx]
+        #             # Upscale the data to highest resolution
+        #             upscaled = resize(
+        #                 real_array,
+        #                 [el * 2**scale for el in real_array.shape],
+        #             )
+        #             # Return upscaled coordinates, the scale, and chunk
+        #             yield (
+        #                 x * 2**scale,
+        #                 y * 2**scale,
+        #                 z,
+        #                 scale,
+        #                 upscaled,
+        #             )
 
 
 global worker
@@ -316,14 +383,12 @@ def dims_update_handler(invar):
         int
     )
 
-    top_left = (
-        int(np.max((corner_pixels[-2, -2], canvas_corners[-2, -2]))),
-        int(np.max((corner_pixels[-2, -1], canvas_corners[-2, -1]))),
-    )
-    bottom_right = (
-        int(np.min((corner_pixels[-1, -2], canvas_corners[-1, -2]))),
-        int(np.min((corner_pixels[-1, -1], canvas_corners[-1, -1]))),
-    )
+    top_left = np.max((corner_pixels, canvas_corners), axis=0)[0, :]
+    bottom_right = np.min((corner_pixels, canvas_corners), axis=0)[1, :]
+
+    # TODO Image.corner_pixels behaves oddly maybe b/c VirtualData
+    if bottom_right.shape[0] > 2:
+        bottom_right[0] = canvas_corners[1, 0]
 
     corners = np.array([top_left, bottom_right], dtype=int)
 
@@ -336,12 +401,12 @@ def dims_update_handler(invar):
 
     # This will consume our chunks and update the numpy "canvas" and refresh
     def on_yield(coord):
-        x, y, z, scale, chunk = coord
-        chunk_size = chunk.shape
-        LOGGER.info(f"Writing chunk to: {(viewer.dims.current_step[0], y, x)}")
-        layer.data[viewer.dims.current_step[0], y : (y + chunk_size[-2]), x : (x + chunk_size[-1])] = chunk[:chunk_size[-2], :chunk_size[-1]]
-        if np.random.rand() < 0.1:
-            layer.refresh()
+        layer = viewer.layers[0]
+        z, y, x, scale, chunk, chunk_size = coord
+        # chunk_size = chunk.shape
+        LOGGER.info(f"Writing chunk with size {chunk_size} to: {(viewer.dims.current_step[0], y, x)}")
+        layer.data[z, y : (y + chunk_size[-2]), x : (x + chunk_size[-1])] = chunk[:chunk_size[-2], :chunk_size[-1]]
+        layer.refresh()
 
     worker.yielded.connect(on_yield)
 
@@ -352,4 +417,5 @@ viewer.camera.events.zoom.connect(dims_update_handler)
 viewer.camera.events.center.connect(dims_update_handler)
 
 
-napari.run()
+# napari.run()
+
